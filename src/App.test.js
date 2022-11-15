b42b680ed7820f4ddf56a46c6d922656cc9a2d49
doubleppereira <cyberafael2005@gmail.com>
2022-11-15 19:15:34 +0000

    Deploying to master from @ doubleppereira/doubleppereira.github.io@15b4eafce18fd924f7b929de598beae6011939d8 🚀

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-jest.mock('react-medium-image-zoom', () => ({ children }) => <>{children}</>);
-jest.mock('react-ga', () => ({ initialize: () => {}, pageview: () => {} }));
-it('renders without crashing', () => {
-  render(<App />);
-});
